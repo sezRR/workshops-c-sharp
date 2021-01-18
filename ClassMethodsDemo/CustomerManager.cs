ffefcefe07c8dc722de540ba9bdb285b08c764eb
szRR <sezer.tetik2005@gmail.com>
2021-01-18 15:39:14 +0300

    Added ClassMethodsDemo workshop

---
+++ ClassMethodsDemo/CustomerManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodsDemo
+{
+     class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine($"-------------------ADD CUSTOMER-------------------");
+
+            Console.WriteLine($"{customer.CustomerId}:");
+            Console.WriteLine($"First Name: {customer.CustomerFirstName}");
+            Console.WriteLine($"Last Name: {customer.CustomerLastName}");
+            Console.WriteLine($"Birth Year: {customer.CustomerBirthYear}");
+            Console.WriteLine($"Customer has been added to database.\n");
+        }
+
+        public void Update(Customer customer)
+        {
+            Console.WriteLine($"-------------------UPDATE CUSTOMER-------------------");
+
+            Console.WriteLine($"{customer.CustomerId}:");
+            Console.WriteLine($"First Name: {customer.CustomerFirstName}");
+            Console.WriteLine($"Last Name: {customer.CustomerLastName}");
+            Console.WriteLine($"Birth Year: {customer.CustomerBirthYear}");
+            Console.WriteLine($"Customer has been updated on database.\n");
+        }
+
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine($"-------------------DELETE CUSTOMER-------------------");
+
+            Console.WriteLine($"{customer.CustomerId}:");
+            Console.WriteLine($"First Name: {customer.CustomerFirstName}");
+            Console.WriteLine($"Last Name: {customer.CustomerLastName}");
+            Console.WriteLine($"Birth Year: {customer.CustomerBirthYear}");
+            Console.WriteLine($"Customer has been deleted on database.\n");
+        }
+
+        public void ListCustomers(params Customer[] customers)
+        {
+            Console.WriteLine($"-------------------CUSTOMERS LIST-------------------");
+
+            if (customers.Length > 0)
+            {
+                foreach (var customer in customers)
+                {
+                    Console.WriteLine($"{customer.CustomerId}:");
+                    Console.WriteLine($"First Name: {customer.CustomerFirstName}");
+                    Console.WriteLine($"Last Name: {customer.CustomerLastName}");
+                    Console.WriteLine($"Birth Year: {customer.CustomerBirthYear}\n");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Customers database is empty!");
+            }
+        }
+    }
+}
---
+++ ClassMethodsDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ClassMethodsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new();
+            customer1.CustomerId = 1;
+            customer1.CustomerFirstName = "Ahmet";
+            customer1.CustomerLastName = "Balcı";
+            customer1.CustomerBirthYear = 1992;
+
+            Customer customer2 = new();
+            customer2.CustomerId = 2;
+            customer2.CustomerFirstName = "Halil";
+            customer2.CustomerLastName = "Kara";
+            customer2.CustomerBirthYear = 1978;
+
+            CustomerManager customerManager = new();
+            customerManager.Add(customer2);
+            customerManager.Update(customer1);
+            customerManager.Delete(customer2);
+            customerManager.ListCustomers(customer1, customer2);
+
+            Console.WriteLine("\n| END OF THE PROCESS |");
+        }
+    }
+
+    class Customer
+    {
+        public int CustomerId { get; set; }
+        public string CustomerFirstName { get; set; }
+        public string CustomerLastName { get; set; }
+        public int CustomerBirthYear { get; set; }
+    }
+}
