36cb431142713df51c0e94de432e87d4ec23e898
szRR <sezer.tetik2005@gmail.com>
2021-01-22 21:08:26 +0300

    Added MyDictionaryClass workshop.

---
+++ MyDictionary/MyDictionaryClass.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionaryClass<TKey, TValue>
+    {
+        public TKey[] _keys;
+        public TValue[] _values;
+
+        public MyDictionaryClass()
+        {
+            _keys = new TKey[0];
+            _values = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] tempKeys = _keys;
+            TValue[] tempValues = _values;
+
+            _keys = new TKey[tempKeys.Length + 1];
+            _values = new TValue[tempValues.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                _keys[i] = tempKeys[i];
+            }
+
+            _keys[_keys.Length - 1] = key;
+
+            for (int i = 0; i < tempValues.Length; i++)
+            {
+                _values[i] = tempValues[i];
+            }
+
+            _values[_values.Length - 1] = value;
+        }
+
+        public int Count
+        {
+            get { return _keys.Length; }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionaryClass<string, int> myDictionaryClass = new MyDictionaryClass<string, int>();
+            myDictionaryClass.Add("First", 1);
+            myDictionaryClass.Add("Second", 2);
+            myDictionaryClass.Add("Third", 3);
+
+            Console.WriteLine(myDictionaryClass.Count); // Output: 3
+
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+            dictionary.Add("First", 1);
+            dictionary.Add("Second", 2);
+            dictionary.Add("Third", 3);
+
+            Console.WriteLine(dictionary.Count); // Output: 3
+        }
+    }
+}
